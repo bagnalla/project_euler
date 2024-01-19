3db81fe30536102710d52b4519d9c94374ca42cb
Alex <ab667712@ohio.edu>
2024-01-19 10:43:01 -0500

    Problem 94 minor improvement

--- ocaml/problem_94/bin/main.ml
+++ ocaml/problem_94/bin/main.ml
@@ -14,28 +14,30 @@ open Z
 let () =
   let sum = ref zero in
 
-  (* Try every [n, n, n-1] and [n, n, n+1] for [n ∈ [2, 333333333]]
-     (so the perimeter doesn't exceed one billion). *)
-  for n = 2 to Int.( / ) 1000000000 3 do
-    let l = of_int n in
+  (* Try every [n, n, n-1] and [n, n, n+1] for odd [n ∈ [3,
+     333333333]] (so [3*n] is odd and the perimeter doesn't exceed one billion). *)
+  let n = ref 3 in
+  while Int.( < ) !n (Int.( / ) 1000000000 3) do
+    let l = of_int !n in
+    
+    (* Semiperimeter [s]. It must be that [pred (~$3 * l)] is even
+       here for this division by 2 to be sound. This is the case when
+       [~$3 * l] is odd, which is true iff [l] is odd, which is why we
+       are only checking odd values of [l].  *)
+    let s = pred (~$3 * l) / ~$2 in
 
-    (* First, we check if the semiperimeter [s] is an integer by
-       checking that [3n +- 1] is even (which is true when [3n] is odd). *)
-    if is_odd @@ ~$3 * l then
+    (* Let [a=b=l] and [c=l-1] and check if the radicand in Heron's
+       formula is a square (and thus the area given by the square
+       root is an integer). *)
+    if perfect_square @@ s * (s - l) * (s - l) * succ (s - l) then
+      sum := !sum + ~$2 * s;
+    let s = succ (~$3 * l) / ~$2 in
+    
+    (* Let [a=b=l] and [c=l+1] and do the same. *)
+    if perfect_square @@ s * (s - l) * (s - l) * pred (s - l) then
+      sum := !sum + ~$2 * s;
 
-      (* Semiperimeter [s]. *)
-      let s = pred (~$3 * l) / ~$2 in
-
-      (* Let [a=b=l] and [c=l-1] and check if the radicand in Heron's
-         formula is a square (and thus the area given by the square
-         root is an integer). *)
-      if perfect_square @@ s * (s - l) * (s - l) * succ (s - l) then
-        sum := !sum + ~$2 * s;
-      let s = succ (~$3 * l) / ~$2 in
-
-      (* Let [a=b=l] and [c=l+1] and do the same. *)
-      if perfect_square @@ s * (s - l) * (s - l) * pred (s - l) then
-        sum := !sum + ~$2 * s;
+    n := Int.( + ) !n 2
   done;
 
   print_endline @@ to_string !sum
